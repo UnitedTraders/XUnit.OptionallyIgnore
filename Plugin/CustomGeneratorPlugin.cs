3a4959d3517c6f2d53d388639461153682165473
chrismckelt <chris@mckelt.com>
2013-09-11 20:40:23 +1000

    init commit
    first commit

---
+++ Plugin/CustomGeneratorPlugin.cs
@@ -0,0 +1,32 @@
+﻿using BoDi;
+using McKeltCustom.Generator.SpecflowPlugin;
+using TechTalk.SpecFlow.Generator.Configuration;
+using TechTalk.SpecFlow.Generator.Plugins;
+using TechTalk.SpecFlow.Generator.UnitTestProvider;
+using TechTalk.SpecFlow.Infrastructure;
+using TechTalk.SpecFlow.UnitTestProvider;
+
+[assembly: GeneratorPlugin(typeof(CustomGeneratorPlugin))]
+
+namespace McKeltCustom.Generator.SpecflowPlugin
+{
+
+    public class CustomGeneratorPlugin : IGeneratorPlugin
+    {
+        public void RegisterDependencies(ObjectContainer container)
+        {
+
+        }
+
+        public void RegisterCustomizations(ObjectContainer container, SpecFlowProjectConfiguration generatorConfiguration)
+        {
+            container.RegisterTypeAs<CustomGeneratorProvider, IUnitTestGeneratorProvider>();
+            container.RegisterTypeAs<CustomXUnitTestRuntimeProvider, IUnitTestRuntimeProvider>();
+        }
+
+        public void RegisterConfigurationDefaults(SpecFlowProjectConfiguration specFlowConfiguration)
+        {
+
+        }
+    }
+}
---
+++ Plugin/LongRunningRegressionTestFactAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TechTalk.SpecFlow;
+using Xunit;
+using Xunit.Sdk;
+
+namespace McKeltCustom.Generator.SpecflowPlugin
+{
+    public class LongRunningRegressionTestFactAttribute : FactAttribute
+    {
+        public const string LongRunningTest = "LongRunningTest";
+        public const string OnlyRunOnBuildServer = "OnlyRunOnBuildServer";
+        public const string LongRunningRegressionTest = "LongRunningRegressionTest";
+        public const string IgnoreLocally = "IgnoreLocally";
+
+        protected override IEnumerable<Xunit.Sdk.ITestCommand> EnumerateTestCommands(Xunit.Sdk.IMethodInfo method)
+        {
+            bool ignore = ScenarioContext.Current.ScenarioInfo.Tags.Contains(LongRunningTest) ||
+                          ScenarioContext.Current.ScenarioInfo.Tags.Contains(OnlyRunOnBuildServer) ||
+                          ScenarioContext.Current.ScenarioInfo.Tags.Contains(LongRunningRegressionTest) ||
+                          ScenarioContext.Current.ScenarioInfo.Tags.Contains(IgnoreLocally);
+
+            if (ignore)
+            {
+                string msg = ScenarioContext.Current.ScenarioInfo.Tags.Aggregate(string.Empty, (current, tag) => current + (tag + " : "));
+
+                yield return new SkipCommand(method, ScenarioContext.Current.ScenarioInfo.Title, "Test Ignored: " + msg);
+            }
+
+            var cmds = base.EnumerateTestCommands(method);
+
+            foreach (var testCommand in cmds)
+            {
+                yield return testCommand;
+            }
+
+        }
+    }
+}
